4cfd482ecb8f98d2cc14fc982eba408d406dca41
marsanla <marcossanzlatore@gmail.com>
2013-08-14 13:16:35 +0200

    add redis and fix things

--- src/Genius13/Notification/Notification.php
+++ src/Genius13/Notification/Notification.php
@@ -51,15 +51,23 @@ class Notification {
 	 */
 	public function create($message, $owner_id = 0, $event = 'general')
 	{
+		$now = time();
+		
 		$data = json_encode(array(
 			'owner_id' => $owner_id,
 			'event' => $event, 
 			'value' => $message,
-			'time' => time(),
+			'time' => $now,
+			'seen' => 0,
 		));
 		
 		// Save in new queue
-		return $this -> redis-> sadd($this -> prefix . ':new:' . $owner_id, $data );
+		if (!$this -> redis-> get($this -> prefix . ':new:' . $owner_id)) {
+			$this -> redis-> set($this -> prefix . ':new:' . $owner_id, 1);
+		} else {
+			$this -> redis-> incr($this -> prefix . ':new:' . $owner_id);	
+		}
+		return $this -> redis-> zadd($this -> prefix . ':' . $owner_id, $now, $data);
 	}
 	
 	/**
@@ -70,20 +78,9 @@ class Notification {
 	 */
 	public function counterNew(Array $owner_ids = array())
 	{
-		return $this -> counter('new', $owner_ids);
+		return $this -> counter($owner_ids, 'new');
 	}
 	
-	/**
-	 * Count old notifications
-	 *
-	 * @param owner_ids Owner user from the notification
-	 * @return integer
-	 */
-	public function counterOld(Array $owner_ids = array())
-	{
-		return $this -> counter('old', $owner_ids);
-	}
-
 	/**
 	 * Count notifications
 	 *
@@ -91,7 +88,7 @@ class Notification {
 	 * @param owner_ids Owner user from the notification
 	 * @return integer
 	 */
-	private function counter($list, Array $owner_ids = array())
+	private function counter(Array $owner_ids = array(), $list = null)
 	{
 		if (empty($owner_ids)) {
 			$owner_ids = array(0);
@@ -101,7 +98,11 @@ class Notification {
 		
 		foreach ($owner_ids as $owner_id) {
 			// Pop from new and add to old queue
-			$count += $this -> redis-> scard($this -> prefix . ':' . $list . ':' . $owner_id);
+			if (isset($list) && $list == 'new') {
+				$count += $this -> redis-> get($this -> prefix . ':' . $list . ':' . $owner_id);	
+			}else{
+				$count += $this -> redis-> get($this -> prefix . ':' . $owner_id);
+			}
 		}
 		
 		return $count;
@@ -113,7 +114,28 @@ class Notification {
 	 * @param owner_ids Owner user from the notification
 	 * @return void
 	 */
-	public function getByOwner(Array $owner_ids = array())
+	public function clear(Array $owner_ids = array())
+	{
+		if (empty($owner_ids)) {
+			$owner_ids = array(0);
+		}
+		
+		$result = 1;
+		
+		foreach ($owner_ids as $owner_id) {
+			$result = $result && $this -> redis-> del($this -> prefix . ':' . $owner_id);
+		}
+		
+		return $result;
+	}
+	
+	/**
+	 * Get notifications
+	 *
+	 * @param owner_ids Owner user from the notification
+	 * @return void
+	 */
+	public function getByOwner(Array $owner_ids = array(), $limit = -1)
 	{
 		if (empty($owner_ids)) {
 			$owner_ids = array(0);
@@ -123,25 +145,37 @@ class Notification {
 		
 		foreach ($owner_ids as $owner_id) {
 			
-			$new = $this -> redis-> smembers($this -> prefix . ':new:' . $owner_id);
-			if ($new) {
-				$result['new'][$owner_id] = $new;	
+			$noti = $this -> redis-> zrevrange($this -> prefix . ':' . $owner_id, 0, ($limit <= 0)? -1 : $limit - 1);
+			if ($noti) {
+								
+				foreach ($noti as $value) {
+
+					$data = json_decode($value);
+					array_push($result, $data);
+					
+					if (!$data->seen) {
+						$data_aux = json_encode(array(
+							'owner_id' => $data->owner_id,
+							'event' => $data->event, 
+							'value' => $data->value,
+							'time' => $data->time,
+							'seen' => 1,
+						));
+						$this -> redis-> zrem($this -> prefix . ':' . $owner_id, $value);
+						$this -> redis-> zadd($this -> prefix . ':' . $owner_id, $data->time, $data_aux);
+					}
+					
+				}	
 			}
 			
-			$old = $this -> redis-> smembers($this -> prefix . ':old:' . $owner_id);
-			if ($old) {
-				$result['old'][$owner_id] = $old;	
-			}
-			
-			if ($new) {
-				$this -> redis -> sadd($this -> prefix . ':old:' . $owner_id, $new);
-				$this -> redis -> srem($this -> prefix . ':new:' . $owner_id, $new);
-			}
+			// New counter to zero
+			$this -> redis-> set($this -> prefix . ':new:' . $owner_id, 0);
 			
 		}
-		
 		return $result;
 		
 	}
+	
+	
 
 }
